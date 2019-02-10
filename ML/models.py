eb32ccaaef728718bcb700b9973dc143785b78eb
t16cs055 <t16cs055@jw09.kki.yamanashi.ac.jp>
2019-02-11 00:53:58 +0900

    [thied]

--- ML/models.py
+++ ML/models.py
@@ -17,13 +17,13 @@ class Type(models.Model):
 class Song(models.Model):
     SongID = models.IntegerField()
     TypeName = models.ForeignKey(Type, db_column = 'TypeName' , on_delete = CASCADE)
-    Name = models.CharField(max_length=50, validators=[MinLengthValidator(1)])
+    SongName = models.CharField(max_length=50, validators=[MinLengthValidator(1)])
     AddDate = models.DateField()
     
     class Meta:
         ordering = ('SongID' , )
     
     def __str__(self):
-        return str(str.SongID + ' > Type: ' + str.TypeName + ' Song Name: ' + str.Name + ' Add Date: ' + str.AddDate)
-    
+        output = (str(self.SongName) + "     :   (" + str(self.TypeName) + ")   ,  楽曲追加日 - " + str(self.AddDate))
+        return  output    
     
