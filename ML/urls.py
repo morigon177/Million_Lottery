9852dbc720cc144cd98d78a92a50c25338677b70
Owner <Owner@LAPTOP-46SB9G7C>
2019-02-11 11:31:35 +0900

    [fifth]

--- ML/urls.py
+++ ML/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'ML'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('registration/logged_out', auth_views.LogoutView.as_view(), name='logout'),
